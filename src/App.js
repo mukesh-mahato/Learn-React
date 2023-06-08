823d1397dfaf54658a6cc8272768a08464b07780
Mukesh <mukeshmahato2618@gmail.com>
2023-06-08 13:01:11 +0545

    Functional Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ClassComponent from './Components/ClassComponent';
+import FunctionalComponent from './Components/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+      <FunctionalComponent />
      <ClassComponent/>
     </div>
   );
