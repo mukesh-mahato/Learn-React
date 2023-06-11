cf0cbf2c81fbe15a0c8e8f1db37165e45d508eeb
Mukesh <mukeshmahato2618@gmail.com>
2023-06-11 10:27:35 +0545

    props as method

--- src/Components/ParentComponent.js
+++ src/Components/ParentComponent.js
@@ -12,14 +12,14 @@ class ParentComponent extends Component {
       this.greetParent = this.greetParent.bind(this)
     }
 
-    greetParent(){
-        alert(`Hello ${this.state.parentName}`)
+    greetParent(childName){
+        alert(`Hello ${this.state.parentName} from ${childName}`)
     }
     
   render() {
     return (
       <div>
-        <ChildComponent />
+        <ChildComponent greetHandler={this.greetParent} />
       </div>
     )
   }
